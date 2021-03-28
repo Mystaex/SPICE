36919b530a9192fb66bd01aec54cdf47b04a768d
Alexander Rollison <AlexanderRollison.CSE@gmail.com>
2021-03-28 16:29:45 -0500

    FB functionality - User Profile
    FB authentication able to be used but haven't stored account information / been able to get information out of firebase yet. Will use get user id and use it as a PK for their info

---
+++ Android_Studio_Projects/app/src/main/java/com/example/spice/ui/login/Member.java
@@ -0,0 +1,38 @@
+package com.example.spice.ui.login;
+
+public class Member
+{
+    private String email;
+    private String password;
+    private String genre;
+    private String profession;
+    private String utaid;
+    private String name;
+    public Member()
+    {
+
+    }
+
+    public Member(String email, String password, String genre, String profession, String utaid, String name)
+    {
+        this.email = email;
+        this.password = password;
+        this.genre = genre;
+        this.profession = profession;
+        this.utaid = utaid;
+        this.name = name;
+    }
+
+    public String getemail(){return email;}
+    public void setemail(String email){this.email = email;}
+    public String getpassword(){return password;}
+    public void setpassword(String password){this.password = password;}
+    public String getgenre(){return genre;}
+    public void setgenre(String genre){this.genre = genre;}
+    public String getprofession(){return profession;}
+    public void setprofession(String profession){this.profession = profession;}
+    public String getutaid(){return utaid;}
+    public void setutaid(String utaid){this.utaid = utaid;}
+    public String getname(){return name;}
+    public void setname(String name){this.name = name;}
+}
---
+++ Android_Studio_Projects/app/src/main/java/com/example/spice/ui/login/login.java
@@ -0,0 +1,161 @@
+/** Information if needed. Here is full link: https://github.com/firebase/snippets-android/blob/8fa42b206795c271810b038687744b2d2ac15357/auth/app/src/main/java/com/google/firebase/quickstart/auth/EmailPasswordActivity.java#L36-L36
+ * Copyright 2021 Google Inc. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.spice.ui.login;
+
+import com.example.spice.ui.main.MainActivity;
+import com.example.spice.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import static java.security.Security.getProviders;
+
+public class login extends AppCompatActivity {
+
+    EditText mEmail, mPassword;
+    Button loginBtn, createBtn;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.submit);
+        createBtn = findViewById(R.id.signup_button);
+
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(login.this, "Create New Account", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), Signup.class));
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String emailValue = mEmail.getText().toString().trim();
+                String passwordValue = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(emailValue))
+                {
+                    mEmail.setError("Email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(passwordValue))
+                {
+                    mPassword.setError("Password is required");
+                    return;
+                }
+
+                if(passwordValue.length() < 6)
+                {
+                    mPassword.setError("Password should be more than 6 characters");
+                    return;
+                }
+
+                signIn(emailValue, passwordValue);
+                if(emailValue.equals("cse3310") && passwordValue.equals("1234567"))
+                {
+                    Toast.makeText(login.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            }
+        });
+    }
+
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+    // [END on_start_check_user]
+
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(null, "signInWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(null, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void sendEmailVerification() {
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = auth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Email sent
+                    }
+                });
+        // [END send_email_verification]
+    }
+
+    private void reload() { }
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null)
+        {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+    }
+
+}
