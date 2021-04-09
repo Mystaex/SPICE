d1d7af2dd8c10e4575e73dc19bc1da90f533d5c3
Robert Fabbro <62273426+PolskaFly@users.noreply.github.com>
2021-04-08 19:28:37 -0500

    No FB

--- Android_Studio_Projects/app/src/main/java/com/example/spice/ui/model_decision/GenreClassifier.java
+++ Android_Studio_Projects/app/src/main/java/com/example/spice/ui/model_decision/GenreClassifier.java
@@ -1,43 +1,43 @@
 package com.example.spice.ui.model_decision;
 import android.graphics.Bitmap;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.ml.modeldownloader.CustomModel;
-import com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions;
-import com.google.firebase.ml.modeldownloader.DownloadType;
-import com.google.firebase.ml.modeldownloader.FirebaseModelDownloader;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+
 import org.tensorflow.lite.Interpreter;
+
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class GenreClassifier {
     private Interpreter interpreter;
+    private Interpreter.Options options = new Interpreter.Options();
 
     private String[] labels = new String[]{"Blues", "Classical", "Country", "Disco", "Hip-Hop",
             "Jazz", "Metal", "Pop", "Reggae", "Rock"};
 
     public GenreClassifier(){
+        try{
+            FileInputStream f_input_stream = new FileInputStream(new File("/src/main/assets/model.tflite"));
+            FileChannel f_channel = f_input_stream.getChannel();
+            MappedByteBuffer tflite_model = f_channel.map(FileChannel.MapMode.READ_ONLY, 0, f_channel.size());
+            interpreter = new Interpreter(tflite_model, options);
+        } catch (Exception e){
+            Log.e("tfliteException", "Error: couldn't load tflite model.", e);
+        }
     }
 
-    private String predict(float input[]){
-        CustomModelDownloadConditions conditions = new CustomModelDownloadConditions.Builder()
-                .requireWifi()  // Also possible: .requireCharging() and .requireDeviceIdle()
-                .build();
-        FirebaseModelDownloader.getInstance()
-                .getModel("Genre-Classifier", DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND, conditions)
-                .addOnSuccessListener(new OnSuccessListener<CustomModel>() {
-                    @Override
-                    public void onSuccess(CustomModel model) {
-                        File modelFile = model.getFile();
-                        if (modelFile != null) {
-                            interpreter = new Interpreter(modelFile);
-                        }
-                    }
-                });
-
+    public String predict(Double input[]){
         int bufferSize = 10 * Float.SIZE / Byte.SIZE;
 
         ByteBuffer modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder());
@@ -48,6 +48,7 @@ public class GenreClassifier {
 
         return labels[maxIndex(probabilities)];
     }
+
 
     private int maxIndex(FloatBuffer array){
         float max = array.get(0);
